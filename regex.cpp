ed5d68aed3e3d4fc0cf945293978da316cea0a6b
Gaith Hallak <gaithhallak@gmail.com>
2017-05-22 18:54:33 +0300

    Implement regular expression tree

--- regex.cpp
+++ regex.cpp
@@ -1,8 +1,6 @@
 #include "regex.h"
 
-#include <string>
-
-Regex::Symbol::Symbol(const std::string & symbol)
+Symbol::Symbol(const std::string& symbol)
 {
 	switch (symbol.length())
 	{
@@ -39,77 +37,189 @@ Regex::Symbol::Symbol(const std::string & symbol)
 			break;
 
 		default:
+			// TODO: change exception
 			throw std::exception();
 	}
 }
 
-Regex::Symbol::Symbol(char c) : Symbol(std::string(1, c))
+std::string Symbol::to_string() const
 {
+	switch (_type)
+	{
+		case Type::EPS:         return "EPSILON";
+		case Type::OPEN_PAREN:  return "(";
+		case Type::CLOSE_PAREN: return ")";
+		case Type::KLEEN_STAR:  return "*";
+		case Type::UNION_OP:    return "|";
+		default:                return _symbol;
+	}
 }
 
-bool Regex::Symbol::operator==(const Symbol & rhs) const
+std::unique_ptr<RegexTree::Node> RegexTree::NodeFactory::create(
+	RegexTree::NodeType type,
+	std::unique_ptr<RegexTree::Node> left,
+	std::unique_ptr<RegexTree::Node> right)
 {
-	return _symbol == rhs._symbol && _type == rhs._type;
+	switch (type)
+	{
+		case NodeType::CONCAT:
+			return std::make_unique<Concat>(std::move(left), std::move(right));
+		case NodeType::UNION:
+			return std::make_unique<Union>(std::move(left), std::move(right));
+		case NodeType::STAR:
+			return std::make_unique<Star>(std::move(left));
+		default:
+			// TODO: throw another exception
+			throw std::exception();
+	}
 }
 
-bool Regex::Symbol::is_epsilon() const
+std::unique_ptr<RegexTree::Node> RegexTree::NodeFactory::create(
+	RegexTree::NodeType type,
+	Symbol symbol)
 {
-	return _type == Regex::Symbol::Type::EPS;
+	if (type == NodeType::LEAF)
+		return std::make_unique<Leaf>(symbol);
+	else
+		// TODO: throw another exception
+		throw std::exception();
 }
 
-bool Regex::Symbol::is_char() const
+RegexTree::RegexTree(const std::string& regex)
 {
-	return _type == Regex::Symbol::Type::CHAR;
-}
-
-bool Regex::Symbol::is_range() const
-{
-	return _type == Regex::Symbol::Type::RANGE;
-}
-
-bool Regex::Symbol::is_kleen_star() const
-{
-	return _type == Regex::Symbol::Type::KLEEN_STAR;
-}
-
-bool Regex::Symbol::is_union_op() const
-{
-	return _type == Regex::Symbol::Type::UNION_OP;
-}
-
-bool Regex::Symbol::is_open_paren() const
-{
-	return _type == Regex::Symbol::Type::OPEN_PAREN;
-}
-
-bool Regex::Symbol::is_close_paren() const
-{
-	return _type == Regex::Symbol::Type::CLOSE_PAREN;
-}
-
-std::string Regex::Symbol::to_string() const
-{
-	return _symbol;
-}
-
-Regex::Regex(const std::string & regex)
-{
+	std::vector<Symbol> symbols;
 	for (std::size_t i = 0; i < regex.length();)
 	{
 		if (i + 2 < regex.length() && regex[i + 1] == '-')
 		{
-			_symbols.emplace_back(regex.substr(i, 3));
+			symbols.emplace_back(regex.substr(i, 3));
 			i += 3;
 		}
 		else
 		{
-			_symbols.emplace_back(regex[i]);
+			symbols.emplace_back(regex[i]);
 			++i;
 		}
 	}
+
+	// Calculate the closing parens indices for each open paren
+	calc_close_index(symbols);
+
+	// Set the root for the RegexTree
+	root = init(symbols, 0, symbols.size());
 }
 
-const std::vector<Regex::Symbol> & Regex::symbols() const
+std::unique_ptr<RegexTree::Node> RegexTree::init(
+	const std::vector<Symbol>& symbols,
+	std::size_t begin,
+	std::size_t end)
 {
-	return _symbols;
+	if (begin >= end)
+	{
+		// TODO: report error
+		throw std::exception();
+	}
+
+	if (begin + 1 == end)
+		return NodeFactory::create(NodeType::LEAF, symbols[begin]);
+
+	if (symbols[begin].is_open_paren())
+	{
+		auto close = close_index[begin];
+
+		// (...)*...
+		if (close + 1 < end && symbols[close + 1].is_kleen_star())
+		{
+			// (...)*...
+			if (close + 2 < end)
+			{
+				return NodeFactory::create(
+					NodeType::CONCAT,
+					init(symbols, begin, close + 2),
+					init(symbols, close + 2, end));
+			}
+			// (...)*
+			else
+			{
+				return NodeFactory::create(NodeType::STAR,
+				                           init(symbols, begin + 1, close));
+			}
+		}
+		// (...)...
+		else
+		{
+			// (...)...
+			if (close + 1 < end)
+			{
+				return NodeFactory::create(
+					NodeType::CONCAT,
+					init(symbols, begin + 1, close),
+					init(symbols, close + 1, end));
+			}
+			// (...)
+			else
+			{
+				return init(symbols, begin + 1, close);
+			}
+		}
+	}
+
+	// ...|...
+	int depth = 0;
+	for (std::size_t i = begin; i < end; ++i)
+	{
+		depth += symbols[i].is_open_paren();
+		depth -= symbols[i].is_close_paren();
+		if (symbols[i].is_union_op() && depth == 0)
+		{
+			return NodeFactory::create(
+				NodeType::UNION,
+				init(symbols, begin, i),
+				init(symbols, i + 1, end));
+		}
+	}
+
+	// .*...
+	if (begin + 1 < end && symbols[begin + 1].is_kleen_star())
+	{
+		// .*...
+		if (begin + 2 < end)
+		{
+			return NodeFactory::create(
+				NodeType::CONCAT,
+				init(symbols, begin, begin + 2),
+				init(symbols, begin + 2, end));
+		}
+		// .*
+		else
+		{
+			return NodeFactory::create(NodeType::STAR,
+			                           init(symbols, begin, begin + 1));
+		}
+	}
+
+	// ...
+	return NodeFactory::create(
+		NodeType::CONCAT,
+		init(symbols, begin, begin + 1),
+		init(symbols, begin + 1, end));
+}
+
+void RegexTree::calc_close_index(const std::vector<Symbol>& symbols)
+{
+	close_index.resize(symbols.size(), symbols.size());
+
+	std::vector<int> stack;
+	for (std::size_t i = 0; i < symbols.size(); ++i)
+	{
+		if (symbols[i].is_open_paren())
+		{
+			stack.emplace_back(i);
+		}
+		else if (symbols[i].is_close_paren())
+		{
+			close_index[stack.back()] = i;
+			stack.pop_back();
+		}
+	}
 }
--- regex.h
+++ regex.h
@@ -2,67 +2,207 @@
 
 #include <string>
 #include <vector>
+#include <memory>
 
-class Regex
+class Symbol
 {
 public:
-	class Symbol
-	{
-	private:
-		enum class Type
-		{
-			EPS,
-			CHAR,
-			RANGE,
-			OPEN_PAREN,
-			CLOSE_PAREN,
-			KLEEN_STAR,
-			UNION_OP
-		};
+	Symbol(const std::string& symbol = "");
 
-	public:
-		Symbol(const std::string & symbol = "");
-		Symbol(char c);
+	Symbol(char c) : Symbol(std::string(1, c))
+	{ }
 
-		// TODO: Should I overload all operators ??
-		// try to use undefined operator
-		bool operator==(const Symbol & rhs) const;
 
-		bool is_epsilon() const;
-		bool is_char() const;
-		bool is_range() const;
-		bool is_kleen_star() const;
-		bool is_union_op() const;
-		bool is_open_paren() const;
-		bool is_close_paren() const;
+	bool operator==(const Symbol& rhs) const
+	{ return _symbol == rhs._symbol && _type == rhs._type; }
 
-		std::string to_string() const;
+	bool operator!=(const Symbol& rhs) const
+	{ return !(*this == rhs); }
 
-	private:
-		std::string _symbol;
-		Type _type;
-	};
 
-public:
-	explicit Regex(const std::string & regex);
+	bool is_epsilon() const
+	{ return _type == Symbol::Type::EPS; }
 
-	const std::vector<Symbol> & symbols() const;
+	bool is_char() const
+	{ return _type == Symbol::Type::CHAR; }
+
+	bool is_range() const
+	{ return _type == Symbol::Type::RANGE; }
+
+	bool is_kleen_star() const
+	{ return _type == Symbol::Type::KLEEN_STAR; }
+
+	bool is_union_op() const
+	{ return _type == Symbol::Type::UNION_OP; }
+
+	bool is_open_paren() const
+	{ return _type == Symbol::Type::OPEN_PAREN; }
+
+	bool is_close_paren() const
+	{ return _type == Symbol::Type::CLOSE_PAREN; }
+
+	std::string to_string() const;
 
 private:
-	std::vector<Symbol> _symbols;
+	enum class Type
+	{
+		EPS,
+		CHAR,
+		RANGE,
+		OPEN_PAREN,
+		CLOSE_PAREN,
+		KLEEN_STAR,
+		UNION_OP
+	};
+
+
+	std::string _symbol;
+	Type _type;
 };
 
 namespace std
 {
 	template<>
-	struct hash<::Regex::Symbol>
+	struct hash<::Symbol>
 	{
-		using argument_type = ::Regex::Symbol;
+		using argument_type = ::Symbol;
 		using result_type = size_t;
 
-		result_type operator()(const argument_type & arg) const
-		{
-			return hash<string>()(arg.to_string());
-		}
+		result_type operator()(const argument_type& arg) const
+		{ return hash<string>()(arg.to_string()); }
 	};
 }
+
+class RegexTree
+{
+private:
+	class Node;
+
+public:
+	RegexTree(const std::string& regex);
+
+private:
+	enum class NodeType
+	{
+		CONCAT, UNION, STAR, LEAF, NODE
+	};
+
+
+	class Node
+	{
+	public:
+		bool is_concat() const
+		{ return type() == NodeType::CONCAT; }
+
+		bool is_union() const
+		{ return type() == NodeType::UNION; }
+
+		bool is_star() const
+		{ return type() == NodeType::STAR; }
+
+		bool is_symbol() const
+		{ return type() == NodeType::LEAF; }
+
+	protected:
+		virtual NodeType type() const
+		{ return NodeType::NODE; }
+	};
+
+	class NodeFactory
+	{
+	public:
+		static std::unique_ptr<Node> create(
+			NodeType type,
+			std::unique_ptr<Node> left,
+			std::unique_ptr<Node> right = nullptr);
+
+		static std::unique_ptr<Node> create(NodeType type, Symbol symbol);
+	};
+
+	class Concat : public Node
+	{
+	public:
+		Concat(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
+			: _right(std::move(right)), _left(std::move(left))
+		{ }
+
+		// const Node* left() const
+		// { return _left.get(); }
+		// const Node* right() const
+		// { return _right.get(); }
+
+	protected:
+		NodeType type() const override
+		{ return NodeType::CONCAT; }
+
+	private:
+		std::unique_ptr<Node> _right;
+		std::unique_ptr<Node> _left;
+	};
+
+	class Union : public Node
+	{
+	public:
+		Union(std::unique_ptr<Node> left, std::unique_ptr<Node> right)
+			: _right(std::move(right)), _left(std::move(left))
+		{ }
+
+		// const Node* left() const
+		// { return _left.get(); }
+		// const Node* right() const
+		// { return _right.get(); }
+
+	protected:
+		NodeType type() const override
+		{ return NodeType::UNION; }
+
+	private:
+		std::unique_ptr<Node> _right;
+		std::unique_ptr<Node> _left;
+	};
+
+	class Star : public Node
+	{
+	public:
+		Star(std::unique_ptr<Node> child) : _child(std::move(child))
+		{ }
+
+		// const Node* child() const
+		// { return _child.get(); }
+
+	protected:
+		NodeType type() const override
+		{ return NodeType::STAR; }
+
+	private:
+		std::unique_ptr<Node> _child;
+	};
+
+	class Leaf : public Node
+	{
+	public:
+		Leaf(Symbol symbol) : _symbol(symbol)
+		{ }
+
+		// Symbol symbol() const
+		// { return _symbol; }
+
+	protected:
+		NodeType type() const override
+		{ return NodeType::LEAF; }
+
+	private:
+		Symbol _symbol;
+	};
+
+
+	std::unique_ptr<Node> init(const std::vector<Symbol>& symbols,
+	                           std::size_t begin,
+	                           std::size_t end);
+
+	void calc_close_index(const std::vector<Symbol>& symbols);
+
+
+	std::unique_ptr<Node> root;
+	std::vector<std::size_t> close_index;
+};
